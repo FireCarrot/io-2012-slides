bf731f36b3a1a94b173bd91c1963a9baa5e3ff68
Minju Kim <minju2.kim@lge.com>
2018-08-30 19:11:40 +0900

    v8 memory analysis

--- slide_config.js
+++ slide_config.js
@@ -1,14 +1,14 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'V8 Memory Analysis',
+    subtitle: '',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/lg_icon.jpeg',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Inconsolata'
@@ -18,8 +18,8 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
+    name: 'Minju Kim',
+    company: 'LG Electronics',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
